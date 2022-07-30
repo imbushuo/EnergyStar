54923401591b7dedb0f7fbe0b494338a60f06a79
Bingxing Wang <i@imbushuo.net>
2022-07-30 02:40:39 -0700

    Project: fix file copy path

--- Package/buildout.ps1
+++ Package/buildout.ps1
@@ -16,13 +16,13 @@ New-Item -ItemType Directory -Force -Path Package/buildout.amd64.release
 ((Get-Content -Path Package/EnergyStar.inx -Raw).Replace('$ARCH$','amd64')) | Set-Content -Path Package/buildout.amd64.release/EnergyStar.inf
 
 # Copy artifacts to buildout folder
-Copy-Item EnergyStar\bin\Release\net6.0\win-arm64\publish\*.* Package/buildout.arm64.release
-Copy-Item EnergyStar\bin\Release\net6.0\win-x64\publish\*.* Package/buildout.amd64.release
+Copy-Item EnergyStar\bin\ReleaseInvisible\net6.0\win-arm64\publish\*.* Package/buildout.arm64.release
+Copy-Item EnergyStar\bin\ReleaseInvisible\net6.0\win-x64\publish\*.* Package/buildout.amd64.release
 
 # Generate Catalog
 . "C:\Program Files (x86)\Windows Kits\10\bin\x86\inf2cat.exe" /driver:Package/buildout.arm64.release /os:10_CO_ARM64
 . "C:\Program Files (x86)\Windows Kits\10\bin\x86\inf2cat.exe" /driver:Package/buildout.amd64.release /os:10_CO_X64
 
 # Package pre-signed CAB
-New-CabinetFile -Name buildout.amd64.release.cab -ContentDirectory Package/buildout.amd64.release -DestinationDir Release -DestinationPath Package
-New-CabinetFile -Name buildout.arm64.release.cab -ContentDirectory Package/buildout.arm64.release -DestinationDir Release -DestinationPath Package
+New-CabinetFile -Name buildout.amd64.release.cab -ContentDirectory Package/buildout.amd64.release -DestinationDir ReleaseInvisible -DestinationPath Package
+New-CabinetFile -Name buildout.arm64.release.cab -ContentDirectory Package/buildout.arm64.release -DestinationDir ReleaseInvisible -DestinationPath Package
