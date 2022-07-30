59672b0a60cf12b1668c3225faf307ce2728c2d2
Bingxing Wang <i@imbushuo.net>
2022-07-29 23:18:15 -0700

    Mandate Windows 11 at least SV1

--- EnergyStar/Program.cs
+++ EnergyStar/Program.cs
@@ -26,6 +26,18 @@ namespace EnergyStar
 
         static void Main(string[] args)
         {
+            // Well, this program only works for Windows Version starting with Cobalt...
+            // Nickel or higher will be better, but at least it works in Cobalt
+            //
+            // In .NET 5.0 and later, System.Environment.OSVersion always returns the actual OS version.
+            if (Environment.OSVersion.Version.Build <= 22000)
+            {
+                Console.WriteLine("E: You are too poor to use this program.");
+                Console.WriteLine("E: Please upgrade to Windows 11 22H2 for best result, and considering ThinkPad Z13 as your next laptop.");
+                // ERROR_CALL_NOT_IMPLEMENTED
+                Environment.Exit(120);
+            }
+
             HookManager.SubscribeToWindowEvents();
             EnergyManager.ThrottleAllUserBackgroundProcesses();
 
